99daa557c8543388458dd1ba5c1c326901d8f6a9
Shamah <shamahfathima2020@gmail.com>
2024-04-17 10:00:00 +0530

    setter and getter

--- src/main/java/com/agronxt/productapi/model/Product.java
+++ src/main/java/com/agronxt/productapi/model/Product.java
@@ -14,4 +14,34 @@ public class Product {
     private Integer quantity;
 
     // Getters and Setters (or use Lombok @Data)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
