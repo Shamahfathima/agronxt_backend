274ac65cf57f30f772d5476fd68189c2b749e2fd
Shamah <shamahfathima2020@gmail.com>
2025-03-27 11:44:34 +0530

    create product entity model

--- src/main/java/com/agronxt/productapi/model/Product.java
+++ src/main/java/com/agronxt/productapi/model/Product.java
@@ -1,5 +1,17 @@
 package com.agronxt.productapi.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+
+    // Getters and Setters (or use Lombok @Data)
 }
