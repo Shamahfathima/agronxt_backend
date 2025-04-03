c55d3aebb99c1cdb93f3e99dd753f21970f3590c
Shamah <shamahfathima2020@gmail.com>
2025-04-03 14:54:34 +0530

    create product service

--- src/main/java/com/agronxt/productapi/controller/ProductController.java
+++ src/main/java/com/agronxt/productapi/controller/ProductController.java
@@ -1,3 +1,46 @@
+package com.agronxt.productapi.controller;
+
+import com.agronxt.productapi.model.Product;
+import com.agronxt.productapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
-    
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
 }
